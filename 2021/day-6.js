200a447cff1c724db1339e27f3290021a7a19790
Chidi Williams <williamschidi1@gmail.com>
2021-12-12 06:12:23 +0000

    Use Array.from to create counters

--- 2021/day-6.js
+++ 2021/day-6.js
@@ -3,30 +3,23 @@ const input = fs.readFileSync(__dirname + '/input.txt').toString();
 
 let list = input.split('\n')[0].split(',').map(Number);
 
-let inputMap = {};
-for (let i = 0; i < 9; i++) {
-  inputMap[i] = 0;
-}
+let inputMap = Array.from({ length: 9 }, () => 0);
 list.forEach((elem) => {
   inputMap[elem]++;
 });
 
 for (let i = 0; i < 256; i++) {
-  const nextMap = {};
+  const nextMap = Array.from({ length: 9 }, () => 0);
 
-  for (let j = 0; j <= 8; j++) {
+  nextMap.forEach((_, j) => {
     if (j > 0) {
       nextMap[j - 1] = inputMap[j];
     }
-  }
+  });
 
   // make 6s of 0
   const numZeroes = inputMap[0];
-  if (nextMap[6]) {
-    nextMap[6] += numZeroes;
-  } else {
-    nextMap[6] = numZeroes;
-  }
+  nextMap[6] += numZeroes;
 
   // push 8s
   nextMap[8] = numZeroes;
